db4e78bc22f1902e2c6c70bc3660322b366dff47
jordiv7 <42815504+jordiv7@users.noreply.github.com>
2022-07-05 18:07:45 -0700

    Update BNO055.c

--- Core/Src/BNO055.c
+++ Core/Src/BNO055.c
@@ -12,9 +12,9 @@ uint8_t BNO055_Init(BNO055 *dev, uint8_t ADR_PIN, I2C_HandleTypeDef *i2cHandle)
     dev->i2cHandle = i2cHandle;
 
     if (ADR_PIN)
-        dev->ADDRESS = BNO055_ADDRESS_HIGH;
+        dev->ADDRESS = BNO055_ADDRESS_HIGH << 1;
     else
-        dev->ADDRESS = BNO055_ADDRESS_LOW;
+        dev->ADDRESS = BNO055_ADDRESS_LOW << 1;
 
     uint8_t errNum = 0;
     uint8_t regData = 0;
@@ -22,14 +22,19 @@ uint8_t BNO055_Init(BNO055 *dev, uint8_t ADR_PIN, I2C_HandleTypeDef *i2cHandle)
 
     // Check if BNO055 is connected
     status = BNO055_readRegister(dev, BNO055_REG_CHIP_ID, &regData);
-    if (status == HAL_BUSY)
+    if ((status == HAL_BUSY) || (status == HAL_ERROR))
     {
-        //	I2C_ClearBusyFla
+    	// delay and check again
+        HAL_Delay(500);
+        status = BNO055_readRegister(dev, BNO055_REG_CHIP_ID, &regData);
+        // if failed, return fail code
+        if ((status == HAL_BUSY) || (status == HAL_ERROR))
+        	return 255;
     }
     errNum += (status != HAL_OK);
 
-    // if (regData != 0xA0)
-    //     return 255;
+    if (regData != 0xA0)
+         return 255;
 
     dev->ACC_X = 0;
     dev->ACC_Y = 0;
@@ -45,7 +50,7 @@ uint8_t BNO055_Init(BNO055 *dev, uint8_t ADR_PIN, I2C_HandleTypeDef *i2cHandle)
     status = BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_X_LSB, dataBuf, 2);
     errNum += (status != HAL_OK);
 
-    dev->MAG_X = (dataBuf[0] << 8) | dataBuf[1];
+    dev->MAG_X = ((int16_t)dataBuf[0] << 8) | dataBuf[1];
 
     return errNum;
 }
@@ -75,15 +80,15 @@ HAL_StatusTypeDef BNO055_readMAG(BNO055 *dev)
     int8_t data[2];
     // Get Data from REG_MAG_DATA_X
     BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_X_LSB, data, 2);
-    dev->MAG_X = (data[0] << 8) | data[1];
+    dev->MAG_X = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from REG_MAG_DATA_Y
     BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_Y_LSB, data, 2);
-    dev->MAG_Y = (data[0] << 8) | data[1];
+    dev->MAG_Y = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from REG_MAG_DATA_X
     BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_Z_LSB, data, 2);
-    dev->MAG_Z = (data[0] << 8) | data[1];
+    dev->MAG_Z = ((int16_t)data[0] << 8) | data[1];
 }
 
 HAL_StatusTypeDef BNO055_readGyro(BNO055 *dev)
@@ -91,15 +96,15 @@ HAL_StatusTypeDef BNO055_readGyro(BNO055 *dev)
     int8_t data[2];
     // Get Data from BNO055_REG_GYR_DATA_X
     BNO055_readRegisters(dev, BNO055_REG_GYR_DATA_X_LSB, data, 2);
-    dev->GYRO_X = (data[0] << 8) | data[1];
+    dev->GYRO_X = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from BNO055_REG_GYR_DATA_Y
     BNO055_readRegisters(dev, BNO055_REG_GYR_DATA_Y_LSB, data, 2);
-    dev->GYRO_Y = (data[0] << 8) | data[1];
+    dev->GYRO_Y = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from BNO055_REG_GYR_DATA_Z
     BNO055_readRegisters(dev, BNO055_REG_GYR_DATA_Z_LSB, data, 2);
-    dev->GYRO_Z = (data[0] << 8) | data[1];
+    dev->GYRO_Z = ((int16_t)data[0] << 8) | data[1];
 }
 
 HAL_StatusTypeDef BNO055_readAccel(BNO055 *dev)
@@ -107,15 +112,15 @@ HAL_StatusTypeDef BNO055_readAccel(BNO055 *dev)
     int8_t data[2];
     // Get Data from BNO055_REG_ACC_DATA_X
     BNO055_readRegisters(dev, BNO055_REG_ACC_DATA_X_LSB, data, 2);
-    dev->ACC_X = (data[0] << 8) | data[1];
+    dev->ACC_X = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from BNO055_REG_ACC_DATA_Y
     BNO055_readRegisters(dev, BNO055_REG_ACC_DATA_Y_LSB, data, 2);
-    dev->ACC_Y = (data[0] << 8) | data[1];
+    dev->ACC_Y = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from BNO055_REG_ACC_DATA_Z
     BNO055_readRegisters(dev, BNO055_REG_ACC_DATA_Z_LSB, data, 2);
-    dev->ACC_Z = (data[0] << 8) | data[1];
+    dev->ACC_Z = ((int16_t)data[0] << 8) | data[1];
 }
 
 HAL_StatusTypeDef BNO055_readLIA(BNO055 *dev)
@@ -123,13 +128,13 @@ HAL_StatusTypeDef BNO055_readLIA(BNO055 *dev)
     int8_t data[2];
     // Get Data from BNO055_REG_LIA_DATA_X_LSB
     BNO055_readRegisters(dev, BNO055_REG_LIA_DATA_X_LSB, data, 2);
-    dev->LIA_X = (data[0] << 8) | data[1];
+    dev->LIA_X = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from BNO055_REG_LIA_DATA_Y_LSB
     BNO055_readRegisters(dev, BNO055_REG_LIA_DATA_Y_LSB, data, 2);
-    dev->LIA_Y = (data[0] << 8) | data[1];
+    dev->LIA_Y = ((int16_t)data[0] << 8) | data[1];
 
     // Get Data from BNO055_REG_LIA_DATA_Z_LSB
     BNO055_readRegisters(dev, BNO055_REG_LIA_DATA_Z_LSB, data, 2);
-    dev->LIA_Z = (data[0] << 8) | data[1];
+    dev->LIA_Z = ((int16_t)data[0] << 8) | data[1];
 }
