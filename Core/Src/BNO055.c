c5ac8ba356cc99998daef3f64d97fce34a8e3144
jordiv7 <42815504+jordiv7@users.noreply.github.com>
2022-07-10 23:34:34 -0700

    Update BNO055.c

--- Core/Src/BNO055.c
+++ Core/Src/BNO055.c
@@ -36,21 +36,64 @@ uint8_t BNO055_Init(BNO055 *dev, uint8_t ADR_PIN, I2C_HandleTypeDef *i2cHandle)
     if (regData != 0xA0)
          return 255;
 
+    // Check IMU mode
+    status = BNO055_readRegister(dev, BNO055_REG_OPR_MODE, &regData);
+    errNum += (status != HAL_OK);
+
+    status = BNO055_readRegister(dev, BNO055_REG_CALIB_STAT, &regData);
+    errNum += (status != HAL_OK);
+
+    // Set IMU mode Page 21 Table 3-5
+    // Set to NDOF
+    regData = (regData & ~0b00001111) | 0b1100;
+    status = BNO055_writeRegister(dev, BNO055_REG_OPR_MODE, &regData);
+    errNum += (status != HAL_OK);
+
+    // Get ACC Config
+    status = BNO055_readRegister(dev, BNO055_REG_ACC_Config, &regData);
+    errNum += (status != HAL_OK);
+
+    // Get GYR C
+    status = BNO055_readRegister(dev, BNO055_REG_GYR_Config_0, &regData);
+    errNum += (status != HAL_OK);
+
+    status = BNO055_readRegister(dev, BNO055_REG_GYR_Config_1, &regData);
+    errNum += (status != HAL_OK);
+
+    // Get MAG C
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_Config, &regData);
+    errNum += (status != HAL_OK);
+
+    // Get Unit Se
+    status = BNO055_readRegister(dev, BNO055_REG_UNIT_SEL, &regData);
+    errNum += (status != HAL_OK);
+
     dev->ACC_X = 0;
     dev->ACC_Y = 0;
     dev->ACC_Z = 0;
     dev->GYRO_X = 0;
     dev->GYRO_Y = 0;
     dev->GYRO_Z = 0;
-    dev->MAG_X = 0;
-    dev->MAG_Y = 0;
-    dev->MAG_Z = 0;
+    dev->MAG_X = 0.0f;
+    dev->MAG_Y = 0.0f;
+    dev->MAG_Z = 0.0f;
 
     int8_t dataBuf[2];
     status = BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_X_LSB, dataBuf, 2);
     errNum += (status != HAL_OK);
 
-    dev->MAG_X = ((int16_t)dataBuf[0] << 8) | dataBuf[1];
+    dev->MAG_X = (((int16_t)dataBuf[1] << 8) | dataBuf[0])/16.0;
+
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_X_LSB, &regData);
+    errNum += (status != HAL_OK);
+
+    int16_t temp = regData;
+
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_X_MSB, &regData);
+    errNum += (status != HAL_OK);
+
+    temp = (regData << 8) | temp;
+    dev->MAG_X = temp / 16.0;
 
     return errNum;
 }
@@ -77,18 +120,23 @@ HAL_StatusTypeDef BNO055_writeRegisters(BNO055 *dev, uint8_t reg, uint8_t *data,
 
 HAL_StatusTypeDef BNO055_readMAG(BNO055 *dev)
 {
+	HAL_StatusTypeDef status;
+
     int8_t data[2];
     // Get Data from REG_MAG_DATA_X
-    BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_X_LSB, data, 2);
-    dev->MAG_X = ((int16_t)data[0] << 8) | data[1];
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_X_LSB, &data[0]);
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_X_MSB, &data[1]);
+    dev->MAG_X = (((int16_t)data[1] << 8) | data[0])/16.0;
 
     // Get Data from REG_MAG_DATA_Y
-    BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_Y_LSB, data, 2);
-    dev->MAG_Y = ((int16_t)data[0] << 8) | data[1];
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_Y_LSB, &data[0]);
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_Y_MSB, &data[1]);
+    dev->MAG_Y = (((int16_t)data[1] << 8) | data[0])/16.0;
 
     // Get Data from REG_MAG_DATA_X
-    BNO055_readRegisters(dev, BNO055_REG_MAG_DATA_Z_LSB, data, 2);
-    dev->MAG_Z = ((int16_t)data[0] << 8) | data[1];
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_Z_LSB, &data[0]);
+    status = BNO055_readRegister(dev, BNO055_REG_MAG_DATA_Z_MSB, &data[1]);
+    dev->MAG_Z = (((int16_t)data[1] << 8) | data[0])/16.0;
 }
 
 HAL_StatusTypeDef BNO055_readGyro(BNO055 *dev)
@@ -137,4 +185,21 @@ HAL_StatusTypeDef BNO055_readLIA(BNO055 *dev)
     // Get Data from BNO055_REG_LIA_DATA_Z_LSB
     BNO055_readRegisters(dev, BNO055_REG_LIA_DATA_Z_LSB, data, 2);
     dev->LIA_Z = ((int16_t)data[0] << 8) | data[1];
+}
+
+HAL_StatusTypeDef BNO055_readEUL(BNO055 *dev)
+{
+	uint8_t data[2];
+	// Get Data
+	BNO055_readRegister(dev, BNO055_REG_EUL_DATA_HEADING_LSB, &data[0]);
+	BNO055_readRegister(dev, BNO055_REG_EUL_DATA_HEADING_MSB, &data[1]);
+	dev->EUL_HEADING = (((int16_t)data[1] << 8) | data[0])/16.0;
+
+	BNO055_readRegister(dev, BNO055_REG_EUL_DATA_ROLL_LSB, &data[0]);
+	BNO055_readRegister(dev, BNO055_REG_EUL_DATA_ROLL_MSB, &data[1]);
+	dev->EUL_ROLL = (((int16_t)data[1] << 8) | data[0])/16.0;
+
+	BNO055_readRegister(dev, BNO055_REG_EUL_DATA_PITCH_LSB, &data[0]);
+	BNO055_readRegister(dev, BNO055_REG_EUL_DATA_PITCH_MSB, &data[1]);
+	dev->EUL_PITCH = (((int16_t)data[1] << 8) | data[0])/16.0;
 }
